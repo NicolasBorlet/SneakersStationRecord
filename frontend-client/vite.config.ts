371a0b6f3ecf25dd1c05a32e6d906e4446e62164
KonyEnBikini <nicolas.borlet7@gmail.com>
2023-05-31 22:35:39 +0200

    work

--- frontend-client/vite.config.ts
+++ frontend-client/vite.config.ts
@@ -20,22 +20,22 @@ export default defineConfig({
         description: "ssr_application",
         icons: [
           {
-            src: "/src/ssr-logo.png",
+            src: "/src/public/ssr-logo.png",
             sizes: "192x192",
             type: "image/png",
           },
           {
-            src: "/src/ssr-logo.png",
+            src: "/src/public//ssr-logo.png",
             sizes: "256x256",
             type: "image/png",
           },
           {
-            src: "/src/ssr-logo.png",
+            src: "/src/public/c/ssr-logo.png",
             sizes: "384x384",
             type: "image/png",
           },
           {
-            src: "/src/ssr-logo.png",
+            src: "/src/public/ssr-logo.png",
             sizes: "512x512",
             type: "image/png",
           },
